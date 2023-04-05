ee1f98f8bb19892b733e421b0f6fc307cea0ed35
Ala-key <almir.abdrasulov@mail.ru>
2023-04-05 15:45:23 +0600

    added pages to nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Link, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/Layout/Layout";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Delivery from "./pages/Delivery";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Hrrgrgrtge</h1>}></Route>
-          <Route path="/about" element={<h1>About</h1>}></Route>
-          <Route path="/contacts" element={<h1>Contacts</h1>}></Route>
-          <Route path="/delivery" element={<h1>Delivery</h1>}></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contacts" element={<Contacts/>}></Route>
+          <Route path="/delivery" element={<Delivery/>}></Route>
 
         </Routes>
       </Layout>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
-import Nav from "./Nav/Nav";
-import logo from "../../src/assets/b-logo.png"
+import Nav from "../Nav/Nav";
+import logo from "../../assets/b-logo.png"
 
 export default function Layout({children}) {
   return (
     <div className="Layout">
       <header>
-        <img src="logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <Nav></Nav>
       </header>
       <aside>
