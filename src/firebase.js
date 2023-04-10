89a2165a2046c8b9fd102ddc068c016cb1f76b85
Ala-key <almir.abdrasulov@mail.ru>
2023-04-10 14:39:57 +0600

    added db

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCyBTPx4N0Zs8ICDpmGSz0iTxOI0CkjVXU",
+  authDomain: "diplom-d6f26.firebaseapp.com",
+  projectId: "diplom-d6f26",
+  storageBucket: "diplom-d6f26.appspot.com",
+  messagingSenderId: "646087854814",
+  appId: "1:646087854814:web:3dea4fcaef5fc95c5c104f",
+  measurementId: "G-9PRSFECD54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -4,10 +4,13 @@ export default function Category() {
 
   const {params} = useMatch("/categories/:slug");
   const categories = [
-    { id: 1, name: "Toyota", slug: "toyota" },
+    { id: 1, name: "Mercedes", slug: "mercedes" },
     { id: 2, name: "Honda", slug: "honda" },
     { id: 3, name: "Ford", slug: "ford" },
-    { id: 4, name: "Chevrolet", slug: "chevrolet" }
+    { id: 4, name: "Chevrolet", slug: "chevrolet" },
+    { id: 5, name: "Audi", slug: "audi" },
+    { id: 6, name: "BMW", slug: "bmw" }
+
   ];
 
   const category = categories.find((category) => category.slug === params.slug)
