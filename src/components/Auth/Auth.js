f23607da6b3cc1bf824c7b47db152d4f8ea0380d
Ala-key <almir.abdrasulov@mail.ru>
2023-05-12 15:53:41 +0600

    Create component Auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -7,29 +7,5 @@ import CartLink from "../CartLink/CartLink";
 import { NavLink } from "react-router-dom";
 
 export default function Auth() {
-  const { user } = useContext(AppContext);
-
-
-  let styles;
-
-  if (user) {
-    const backgroundImageUrl = user && user.photoURL ? `url(${user.photoURL})` : null;
-    styles = {
-      backgroundImage: backgroundImageUrl,
-      display: "block"
-      // и другие свойства
-    }
-  }
-
-
-  return (
-    <div className="Auth">
-      <NavLink to={"/orders"}>    
-          <div className="accountImage mar-right" style={styles}></div>
-      </NavLink>
-      {user ? <CartLink  className="mar-right"/> : null}
-      {!user ? <button onClick={logIn} className="signIn">Sign in</button> : null}
-      {user ? <button className="signOut" onClick={logOut}><ImExit /></button> : null}
-    </div>
-  );
+  
 }
