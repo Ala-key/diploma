f1d4a8e382cb45594555d20e277a364acda15cd7
Ala-key <almir.abdrasulov@mail.ru>
2023-05-12 15:49:11 +0600

    Calculate total cart items and add navigation

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -7,4 +7,8 @@ import { useNavigate } from "react-router";
 export default function AddToCart({ product }) {
   const { cart, setCart } = useContext(AppContext);
   const [display, setDisplay] = useState("none");
+
+
+  const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
+  const navigate = useNavigate();
 }
