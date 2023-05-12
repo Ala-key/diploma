7d1e20b37f2180463d00ccd5ab0c4a22f490b5b1
Ala-key <almir.abdrasulov@mail.ru>
2023-05-12 15:11:08 +0600

    function using the useState hook

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -4,30 +4,6 @@ import { AppContext } from "../../App";
 import { categoryCollection } from "../../firebase";
 
 export default function AddCategory() {
-  const [category, SetCategory] = useState("");
-  const { user } = useContext(AppContext);
-
-
-  if(!user || !user.isAdmin){ return null;}
-
-  function onChangeCategory(event) {
-    SetCategory(event.target.value);
-  }
-
-
-  function onAddCategory() {
-     addDoc(categoryCollection, {name: category.trim(), slug: category.trim().replaceAll(" ","-").toLocaleLowerCase()}).then(() => {SetCategory("")});
-
-  }
-
-
-  return (
-    <div className="AddCategory">
-      <input type="text"
-        size="15"
-        value={category}
-        placeholder="Category name"
-        onChange={onChangeCategory} />
-      <button onClick={onAddCategory}>+</button>
-    </div>   
+   const [category, SetCategory] = useState("");
+    
 }
