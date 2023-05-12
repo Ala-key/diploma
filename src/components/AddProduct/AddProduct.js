cac1148d8fdc9bd5cadf4de7b81e5ba3e322a309
Ala-key <almir.abdrasulov@mail.ru>
2023-05-12 15:46:08 +0600

    Create AddProduct component with form new product

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,15 +1,15 @@
 export default function AddProduct() {
   return (
     <div className="AddProduct">
-        <form>
-          <h3>Create a new product</h3>
-          <label>
-            Name: <input type="text" name="name" required/>
-          </label>
-          <label>
-            Price: <input type="number" name="price" required min={0}/>
-          </label>
-        </form>
+      <form>
+        <h3>Create a new product</h3>
+        <label>
+          Name: <input type="text" name="name" required />
+        </label>
+        <label>
+          Price: <input type="number" name="price" required min={0} />
+        </label>
+      </form>
     </div>
   )
 }
