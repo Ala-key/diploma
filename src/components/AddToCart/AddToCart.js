86e659b1941b746608940836d0da065ab1d7eeae
Ala-key <almir.abdrasulov@mail.ru>
2023-05-15 14:43:18 +0600

    check netlify

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { AppContext } from "../../App";
 import "./AddToCart.css";
 import Notification from "../Notification/Notification";
-import { useNavigate } from "react-router";
+
 
 export default function AddToCart({ product }) {
   const { cart, setCart } = useContext(AppContext);
