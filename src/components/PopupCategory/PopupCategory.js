73bb96a09d244d5a9fcb6dbd6e0998db2d7d4606
Ala-key <almir.abdrasulov@mail.ru>
2023-05-18 18:36:40 +0600

    Check netlify

--- src/components/PopupCategory/PopupCategory.js
+++ src/components/PopupCategory/PopupCategory.js
@@ -1,33 +1,57 @@
 import "./PopupCategory.css";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 
-export function PopupCategory () {
+export function PopupCategory() {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const [heightState, setHeightState] = useState(100);
+
+  function handleMouseMove(event) {
+
+    console.log("Hello");
+
+    const x = event.clientX;
+    const y = event.clientY;
+
+
+      setHeightState((prev) => prev - 1);
+      console.log(heightState);
+      const popup = document.querySelector(".PopupCategory");
+      popup.style.height = heightState + "vh";
+
+
+    setCoordinates({ x, y });
+  }
+
+
   return (
-    <div className="PopupCategory">
-        <nav className="Nav">
-      <ul>
-        <li className="NavItem">
-          {/* <li><a href="./home.html">Home</a></li> */}
-          <NavLink to="/">Home</NavLink>
-        </li>
+    <div className="PopupCategory" onTouchMove={handleMouseMove}  >
+      <nav className="Nav">
+        <ul>
+          <li className="NavItem">
+            {/* <li><a href="./home.html">Home</a></li> */}
+            <NavLink to="/">Home</NavLink>
+          </li>
 
-        {/* <li><a href="./about.html">About</a></li> */}
-        <li className="NavItem">
-          <NavLink to="/about">About</NavLink>
-        </li>
+          {/* <li><a href="./about.html">About</a></li> */}
+          <li className="NavItem">
+            <NavLink to="/about">About</NavLink>
+          </li>
 
-        {/* <li><a href="./contacts.html">Contacts</a></li> */}
-        <li className="NavItem">
-          <NavLink to="/contacts">Contacts</NavLink>
-        </li>
-        {/* <li><a href="./delivery.html">Delivery</a></li> */}
-        <li className="NavItem">
-          <NavLink to="/delivery">Delivery</NavLink>
-        </li>
-      </ul>
-    </nav>
+          {/* <li><a href="./contacts.html">Contacts</a></li> */}
+          <li className="NavItem">
+            <NavLink to="/contacts">Contacts</NavLink>
+          </li>
+          {/* <li><a href="./delivery.html">Delivery</a></li> */}
+          <li className="NavItem">
+            <NavLink to="/delivery">Delivery</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <button  id="open-garage-door" >Click me X - {coordinates.x}  Y - {coordinates.y}</button>
+
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,55 @@
-export default function Home() {
-  return(
-    <h1>Hello</h1>
-  )
-}
+
+const Home = () => {
+  // const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+
+  // // document.addEventListener("mousemove", function(event) {
+  // //   const x = event.clientX;
+  // //   const y = event.clientY;
+  // //   setCoordinates({ x, y });
+  // // });
+
+
+  // // document.addEventListener("mouseup", function(event) {
+  // //   console.log("Hello");
+  // // });
+
+  // let value = 0;
+
+  // useEffect(() => {
+  //   const handleMouseUp = (event) => {
+
+          
+  //         let div = document.querySelector(".clickingdiv");
+
+      
+  //         div.style.transform = `translate(0px, -${value}px)`;
+
+  //         value += 100;
+  //   };
+
+  //   document.addEventListener("mouseup", handleMouseUp);
+
+  //   return () => {
+  //     document.removeEventListener("mouseup", handleMouseUp);
+  //   };
+  // }, []);
+
+
+
+  // // function Clicking() {
+  // //   console.log("Hello");
+  // //   // let div = document.querySelector(".clickingdiv");
+
+  // //   // div.style.height = coordinates.y + "px";
+  // // }
+
+
+  return (
+    <div>
+      <div className='clickingdiv'>Mouse Click</div>
+    </div>
+  );
+};
+
+export default Home;
