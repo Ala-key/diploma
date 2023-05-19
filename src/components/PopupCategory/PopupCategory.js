75cd91fa7970d8f487892ea10dac6ef0c6781ccf
Ala-key <almir.abdrasulov@mail.ru>
2023-05-20 00:56:15 +0600

    added function check open function if yes we remove class and change toogle on false in start position

--- src/components/PopupCategory/PopupCategory.js
+++ src/components/PopupCategory/PopupCategory.js
@@ -3,5 +3,18 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 export function PopupCategory({ open, toggle, toggleFalse }) {
-     
+  
+  const [translateState, setTranslateState] = useState(0);
+
+
+
+  function onClosePopup() {
+     if (translateState === 0) {
+      document.querySelector(".PopupCategory").classList.remove("open");
+      toggle();
+     }    
+  }
+
+  
+
 }
