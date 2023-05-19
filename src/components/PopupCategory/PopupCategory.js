9b0cf602b139fb15c53cf8041e75d1f4f2c75821
Ala-key <almir.abdrasulov@mail.ru>
2023-05-20 00:53:46 +0600

    added popup component

---
+++ src/components/PopupCategory/PopupCategory.js
@@ -0,0 +1,7 @@
+import "./PopupCategory.css";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+export function PopupCategory({ open, toggle, toggleFalse }) {
+     
+}
