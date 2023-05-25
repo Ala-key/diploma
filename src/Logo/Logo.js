5774e9e9987334c17ebc68c1f1eadfecf4f87199
Ala-key <almir.abdrasulov@mail.ru>
2023-05-25 19:06:00 +0600

    add link in logo to home

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import logo from "../assets/b-logo.png"
 
 
@@ -5,7 +6,9 @@ import logo from "../assets/b-logo.png"
 export default function Logo() {
   return (
     <div className="Logo">
-        <img src={logo} alt="logo" id="logo" />
+      <NavLink to="/">
+          <img src={logo} alt="logo" id="logo" />
+      </NavLink>
     </div>
   )
 }
